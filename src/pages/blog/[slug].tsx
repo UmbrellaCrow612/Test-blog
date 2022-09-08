f77980b215d56b3796731b40fadffa6b4a7f98f9
yousaf wazir <umbrellaman612@gmail.com>
2022-09-08 14:29:48 +0100

    feat: added blog and blog page

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+const Page: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>slug</title>
+          </Head>
+          Slug
+    </>
+  )
+}
+export default Page
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+const index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>index</title>
+      </Head>
+      Blog
+    </>
+  )
+}
+export default index
+
+
