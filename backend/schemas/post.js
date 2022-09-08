7005addc8e22e23488c8eb50ee32cc64bc34f59a
yousaf wazir <umbrellaman612@gmail.com>
2022-09-08 10:33:20 +0100

    studio: added all schema types

---
+++ backend/schemas/post.js
@@ -0,0 +1,98 @@
+export default {
+  title: 'Post', // Visual name
+  name: 'post', // name when querying
+  type: 'document',
+  fields: [
+    // contains all the things inside of it
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' },
+    },
+    {
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+    },
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string',
+    },
+    {
+      title: 'Poster',
+      name: 'poster',
+      type: 'image',
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+          options: {
+            isHighlighted: true, // <-- make this field easily accessible
+          },
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
+    },
+    {
+      title: 'Release date',
+      name: 'release',
+      type: 'date',
+    },
+    {
+      title: 'Edited At',
+      name: 'edited',
+      type: 'date',
+    },
+    {
+      title: 'Content',
+      name: 'content',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative text',
+              description: `Some of your visitors cannot see images, 
+            be they blind, color-blind, low-sighted; 
+            alternative text is of great help for those 
+            people that can rely on it to have a good idea of 
+            what\'s on your page.`,
+              options: {
+                isHighlighted: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+    },
+  ],
+}
